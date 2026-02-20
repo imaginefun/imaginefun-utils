56ffda346b248f878935259d543c96245c5ed4c6
Camden Cove <me@camdencove.com>
2026-02-20 03:31:55 -0800

    implement panorama screen

--- src/main/java/net/imaginefun/ImagineFunUtils.java
+++ src/main/java/net/imaginefun/ImagineFunUtils.java
@@ -2,12 +2,18 @@ package net.imaginefun;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
+import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.imaginefun.networking.GameTestAddMarkerPayload;
 import net.imaginefun.networking.HandshakePayload;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ImagineFunUtils implements ModInitializer {
+
 	public static final String MOD_ID = "imaginefunutils";
 
 	// This logger is used to write text to the console and the log file.
@@ -19,5 +25,14 @@ public class ImagineFunUtils implements ModInitializer {
 	public void onInitialize() {
 		PayloadTypeRegistry.playS2C().register(GameTestAddMarkerPayload.TYPE, GameTestAddMarkerPayload.STREAM_CODEC);
 		PayloadTypeRegistry.playC2S().register(HandshakePayload.TYPE, HandshakePayload.STREAM_CODEC);
+
+		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(mod ->
+            ResourceLoader.registerBuiltinPack(
+                    Identifier.fromNamespaceAndPath(MOD_ID, MOD_ID),
+                    mod,
+                    Component.literal("ImagineFunUtils Resource Pack"),
+                    PackActivationType.ALWAYS_ENABLED
+            )
+		);
 	}
 }
